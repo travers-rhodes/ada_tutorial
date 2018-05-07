820daa80464907e719f5743f4a8ddf5ea86a20f0
travers-rhodes <traversr@andrew.cmu.edu>
2018-05-07 13:37:58 -0400

    shakes a little, but I think it works

--- scripts/planner.py
+++ scripts/planner.py
@@ -8,21 +8,12 @@ import numpy as np
 import time
 import adapy
 import transforms3d as t3d
+import threading
 
-from tf.msgs import tfMessage
-from geometry_msgs.msgs import TransformStamped, Transform, Vectory3
+from std_msgs.msg import Header
+from tf.msg import tfMessage
+from geometry_msgs.msg import TransformStamped, Transform, Vector3
 
-rospy.init_node('adapy_tracker', anonymous=True)
-parser = argparse.ArgumentParser(description='node to track the position given by a rostopic')
-parser.add_argument('-s', '--sim', action='store_true',
-                        help='simulation mode')
-parser.add_argument('-v', '--viewer', nargs='?', const=True,
-                        help='attach a viewer of the specified type')
-parser.add_argument('--env-xml', type=str,
-                        help='environment XML file; defaults to an empty environment')
-parser.add_argument('--debug', action='store_true',
-                        help='enable debug logging')
-args = parser.parse_args()
 
 class PositionCache:
   def __init__(self):
@@ -38,7 +29,7 @@ class PositionCache:
     self.latest_mouth_transform = mesg.transforms[0]
 
 class Tracker:
-  def __init__(self)
+  def __init__(self, args):
     RaveInitialize(True)
     misc.InitOpenRAVELogging()
     
@@ -48,7 +39,7 @@ class Tracker:
         env_path=args.env_xml
     )
     
-    self.manip = robot.SetActiveManipulator("Mico")
+    self.manip = self.robot.SetActiveManipulator("Mico")
     self.manip_rob = interfaces.BaseManipulation(self.robot) # create the interface for basic manipulation programs
 
     # even though it's not obvious how we use this, we need to initialize the IKModel
@@ -61,33 +52,49 @@ class Tracker:
     self.pos_cache = PositionCache()
 
     # move to init position 
-    self.move_to_target([[ 0.21676946, -0.2959789,   0.552686472]], timeoutSecs=0, allowRotation=True)
+    self.move_to_target(np.array([ 0.21676946, -0.2959789,   0.552686472]), timeoutSecs=0, allowRotation=True)
 
+  def follow_mouth(self,timeoutSecs):
     # start tracking the actual mouth
     while not rospy.is_shutdown():
-      self.track_target()
+      self.track_target(timeoutSecs)
+    
 
+  # return an np.array of the [x,y,z] target mouth location
+  # in the coordinate frame of the robot base
   def get_target_loc(self):
-    if self.pos_cache is None:
+    if self.pos_cache.latest_mouth_transform is None:
       return None
     t =  self.pos_cache.latest_mouth_transform.transform.translation
     return np.array([t.x, t.y, t.z])
 
+  # generate the rotation matrix corresponding to the desired end-effector rotation
   def generate_target_rotmat(self):
     roty = t3d.quaternions.quat2mat([0,np.sqrt(2), 0, np.sqrt(2)])
     rotx = t3d.quaternions.quat2mat([np.sqrt(2),0, 0, np.sqrt(2)])
     rot = roty.dot(rotx)
     return rot
 
+  # compute and move toward the target mouth location
+  # only move for at most timeoutSecs,   
+  # if allowRotation is set to True, allow the robot end effector to rotate along the x,y axes. 
+  # Otherwise, only allow rotation around the z-axis
   def track_target(self, timeoutSecs, allowRotation=False):
     endLoc = self.get_target_loc() 
     if endLoc is None:
       rospy.sleep(timeoutSecs)
+      return
     self.move_to_target(endLoc, timeoutSecs, allowRotation=False)
 
-  def move_to_target(self, endLoc, timeoutSecs, allowRotation=False)
+  # for at most timeoutSecs, compute and move toward the input endLoc
+  # endLoc must be a length 3 np.array
+  # if allowRotation is set to True, allow the robot end effector to rotate along the x,y axes. 
+  # Otherwise, only allow rotation around the z-axis
+  def move_to_target(self, endLoc, timeoutSecs, allowRotation=False):
     with self.env:
-      Tgoal = np.concatenate((np.concatenate((self.rot, np.transpose(endLoc)), axis=1),[[0,0,0,1]]), axis=0)
+      print(endLoc)
+      print(endLoc.shape)
+      Tgoal = np.concatenate((np.concatenate((self.rot, np.transpose([endLoc])), axis=1),[[0,0,0,1]]), axis=0)
       constrainttaskmatrix=np.eye(4)
       constraintmatrix = np.linalg.inv(self.manip.GetTransform())
       constrainterrorthresh = 0.005 # copied from tutorial
@@ -95,30 +102,47 @@ class Tracker:
       if not allowRotation:
        # don't let any x,y rotations happen
        constraintfreedoms = [1,1,0,0,0,0] 
-      manip_rob.MoveToHandPosition(matrices=[Tgoal],maxiter=3000,maxtries=1,seedik=40,
+      self.manip_rob.MoveToHandPosition(matrices=[Tgoal],maxiter=3000,maxtries=1,seedik=40,
         constraintfreedoms=constraintfreedoms,
         constraintmatrix=constraintmatrix, 
         constrainttaskmatrix=constrainttaskmatrix,
         constrainterrorthresh=constrainterrorthresh,
         steplength=0.002)
-    robot.WaitForController(timeoutSecs) # wait the timeout amount before choosing a new target
+    self.robot.WaitForController(timeoutSecs) # wait the timeout amount before choosing a new target
 
 if __name__=="__main__":
+  # parse input arguments
+  parser = argparse.ArgumentParser(description='node to track the position given by a rostopic')
+  parser.add_argument('-s', '--sim', action='store_true',
+                          help='simulation mode')
+  parser.add_argument('-v', '--viewer', nargs='?', const=True,
+                          help='attach a viewer of the specified type')
+  parser.add_argument('--env-xml', type=str,
+                          help='environment XML file; defaults to an empty environment')
+  parser.add_argument('--debug', action='store_true',
+                          help='enable debug logging')
+  args = parser.parse_args()
+ 
+  # initialize the ros node 
+  rospy.init_node('adapy_tracker', anonymous=True)
   # start tracker running and start following locations
-  t = Tracker()
+  t = Tracker(args)
+  from threading import Thread
+  thread = Thread(target=t.follow_mouth, args = (0,))
+  thread.start()
   poses =[ 
-                [[ 0.21676946, -0.2959789,   0.552686472]],
-                [[ 0.21676946, -0.2959789,   0.52686472]],
-                [[ 0.21676946, -0.2959789,   0.42686472]],
-                [[ 0.31676946, -0.1959789,   0.42686472]],
-                [[ 0.31676946, -0.3959789,   0.42686472]]]
-  poses = []
+                [ 0.21676946, -0.2959789,   0.552686472],
+                [ 0.21676946, -0.2959789,   0.52686472],
+                [ 0.21676946, -0.2959789,   0.42686472],
+                [ 0.31676946, -0.1959789,   0.42686472],
+                [ 0.31676946, -0.3959789,   0.42686472]]
   pub = rospy.Publisher('feedbot/mouth_pose', tfMessage, queue_size=10)
-  h = std_msgs.msg.Header()
+  h = Header()
   h.stamp = rospy.Time.now()
   for pose in poses:
-    mesg = tfMessage([TransformStamped(h, "dummy_frame", Transform(pose, None))])
+    mesg = tfMessage([TransformStamped(h, "dummy_frame", Transform(Vector3(pose[0], pose[1], pose[2]), None))])
     pub.publish(mesg)
     rospy.sleep(1)
+  thread.join()
 
 
