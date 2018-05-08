f2830bc344b72c47d24d871f55027126ea77597f
travers-rhodes <traversr@andrew.cmu.edu>
2018-05-08 00:21:07 -0400

    have robot move in straight line :heart_eyes:

--- scripts/planner_bad.py
+++ scripts/planner_bad.py
@@ -8,42 +8,30 @@ import numpy as np
 import time
 import adapy
 import transforms3d as t3d
-
-import transform_helpers as th
+import threading
 
 from std_msgs.msg import Header
-from geometry_msgs.msg import Point
+from tf.msg import tfMessage
+from geometry_msgs.msg import TransformStamped, Transform, Vector3
 
-point_topic = "/DO/inferencePointOut"
-x_dist = 0.41676946
-
-class CameraCalibration:
-  def __init__(self):
-    self.camera_to_robot =  np.array([[0,0,1,-0.8],[-1,0,0,-0.4],[0,-1,0,0.55],[0,0,0,1]])
-    self.camera_to_robot = np.eye(4)
-
-  # point should be a length 4 np.array giving the location of the target point in the camera frame
-  def convert_to_robot_frame(self, point):
-    return self.camera_to_robot.dot(point.transpose())
 
 class PositionCache:
   def __init__(self):
-    self.target_listener = rospy.Subscriber(point_topic, Point, self.target_listener_callback)
+    self.target_listener = rospy.Subscriber("/feedbot/mouth_pose", tfMessage, self.target_listener_callback)
     # save off the latest mouth pose tf object seen by the vision system in a cached value
     # this object has
     # * Header header (int seq; time stamp; string frame_id)
     # * string child_frame_id
     # * geometry_msgs/Transform transform (Vectory3 translation; Quaternion rotation)
-    self.latest_mouth_position = None
+    self.latest_mouth_transform = None
 
   def target_listener_callback(self, mesg):
-    self.latest_mouth_position = mesg
+    self.latest_mouth_transform = mesg.transforms[0]
 
 class Tracker:
   def __init__(self, args):
     RaveInitialize(True)
     misc.InitOpenRAVELogging()
-    
     
     self.env, self.robot = adapy.initialize(
         sim=args.sim,
@@ -54,34 +42,31 @@ class Tracker:
     self.manip = self.robot.SetActiveManipulator("Mico")
     self.manip_rob = interfaces.BaseManipulation(self.robot) # create the interface for basic manipulation programs
 
-    # even though it's not obvious how we use this, we need to initialize the IKModel on self.robot
+    # even though it's not obvious how we use this, we need to initialize the IKModel
     ikmodel = databases.inversekinematics.InverseKinematicsModel(self.robot,iktype=IkParameterization.Type.Transform6D)
     if not ikmodel.load():
       ikmodel.autogenerate()
-    
+
     self.rot = self.generate_target_rotmat()
 
     self.pos_cache = PositionCache()
-    self.cameraCalib = CameraCalibration()
 
     # move to init position 
-    self.move_to_target(np.array([x_dist, -0.2959789,   0.552686472]), timeoutSecs=0, constrainMotion=False)
+    self.move_to_target(np.array([ 0.21676946, -0.2959789,   0.552686472]), timeoutSecs=0, allowRotation=True)
 
   def follow_mouth(self,timeoutSecs):
     # start tracking the actual mouth
     while not rospy.is_shutdown():
       self.track_target(timeoutSecs)
+    
 
   # return an np.array of the [x,y,z] target mouth location
   # in the coordinate frame of the robot base
   def get_target_loc(self):
-    if self.pos_cache.latest_mouth_position is None:
+    if self.pos_cache.latest_mouth_transform is None:
       return None
-    t =  self.pos_cache.latest_mouth_position
-    point_in_camera_frame = np.array([t.x, t.y, t.z, 1])
-    point_in_robot_frame = self.cameraCalib.convert_to_robot_frame(point_in_camera_frame)
-    print(point_in_robot_frame)
-    return point_in_robot_frame[0:3]
+    t =  self.pos_cache.latest_mouth_transform.transform.translation
+    return np.array([t.x, t.y, t.z])
 
   # generate the rotation matrix corresponding to the desired end-effector rotation
   def generate_target_rotmat(self):
@@ -89,25 +74,12 @@ class Tracker:
     rotx = t3d.quaternions.quat2mat([np.sqrt(2),0, 0, np.sqrt(2)])
     rot = roty.dot(rotx)
     return rot
-  
-  # return a boolean for whether the end-effector is already at the endLoc target
-  # if the end-effector is already close enough to the target, then there's no need
-  # to move the end-effector to the target
-  def is_close_enough_to_target(self, endLoc):
-    return th.distance(self.get_cur_loc(), endLoc) < 0.001
-
-  def get_cur_loc(self):
-    Tee = self.manip.GetEndEffectorTransform()
-    curLoc = Tee[0:3,3]
-    return(curLoc)
-
-
 
   # compute and move toward the target mouth location
   # only move for at most timeoutSecs,   
-  # if constrainMotion is set to False, don't allow the robot end effector to rotate, and only allow linear motion toward the goal
-  # otherwise, don't constrain motion
-  def track_target(self, timeoutSecs, constrainMotion=True):
+  # if allowRotation is set to True, allow the robot end effector to rotate along the x,y axes. 
+  # Otherwise, only allow rotation around the z-axis
+  def track_target(self, timeoutSecs, allowRotation=False):
     endLoc = self.get_target_loc() 
     if endLoc is None:
       if timeoutSecs == 0:
@@ -115,13 +87,31 @@ class Tracker:
       else:
         rospy.sleep(timeoutSecs)
       return
-    self.move_to_target(endLoc, timeoutSecs, constrainMotion=True)
+    self.move_to_target(endLoc, timeoutSecs, allowRotation=False)
+
+  # return a boolean for whether the end-effector is already at the endLoc target
+  # if the end-effector is already close enough to the target, then there's no need
+  # to move the end-effector to the target
+  def is_close_enough_to_target(self, endLoc):
+    return self.distance_to_target(endLoc) < 0.001
+
+  def distance_to_target(self, endLoc):
+    curLoc = self.get_cur_loc()
+    diff = endLoc - curLoc
+    dist = np.sqrt(sum(diff**2))
+    print(dist)
+    return dist
+
+  def get_cur_loc(self):
+    Tee = self.manip.GetEndEffectorTransform()
+    curLoc = Tee[0:3,3]
+    return(curLoc)
 
   # for at most timeoutSecs, compute and move toward the input endLoc
   # endLoc must be a length 3 np.array
-  # if constrainMotion is set to False, don't allow the robot end effector to rotate, and only allow linear motion toward the goal
-  # otherwise, don't constrain motion
-  def move_to_target(self, endLoc, timeoutSecs, constrainMotion=False):
+  # if allowRotation is set to True, allow the robot end effector to rotate along the x,y axes. 
+  # Otherwise, only allow rotation around the z-axis
+  def move_to_target(self, endLoc, timeoutSecs, allowRotation=False):
     if self.is_close_enough_to_target(endLoc):
       if timeoutSecs == 0:
         rospy.sleep(1)
@@ -134,23 +124,18 @@ class Tracker:
       Tgoal = np.concatenate((np.concatenate((self.rot, np.transpose([endLoc])), axis=1),[[0,0,0,1]]), axis=0)
       print("moving to", Tgoal)
       constrainttaskmatrix=np.eye(4)
-      constraintmatrix = np.eye(4) 
-      constrainterrorthresh = 0.1 # tutorial had 0.005
-      constraintfreedoms = [0,0,0,0,0,0] # don't penalize any direction of motion
-      if constrainMotion:
-        # don't let any x,y,z rotations happen
-        # don't let the robot move in the x,y directions in the task frame (only let it move in the z direction)
-        constraintfreedoms = [1,1,1,1,1,0]
-        motion_frame = th.get_motion_frame_matrix(self.manip.GetTransform(), endLoc)
-        constrainttaskmatrix = np.eye(4) 
-        constraintmatrix = np.linalg.inv(motion_frame)
-      
+      constraintmatrix = np.linalg.inv(self.manip.GetTransform())
+      constrainterrorthresh = 0.005 # copied from tutorial
+      constraintfreedoms = [0,0,0,0,0,0] # based on parsing the tutorial. Not sure why it's not 0,0,1,...
+      if not allowRotation:
+       # don't let any x,y rotations happen
+       constraintfreedoms = [1,1,0,0,0,0] 
       self.manip_rob.MoveToHandPosition(matrices=[Tgoal],maxiter=3000,maxtries=10,seedik=40,
         constraintfreedoms=constraintfreedoms,
         constraintmatrix=constraintmatrix, 
         constrainttaskmatrix=constrainttaskmatrix,
         constrainterrorthresh=constrainterrorthresh,
-        steplength=0.002) # tutorial had 0.002
+        steplength=0.001)
     self.robot.WaitForController(timeoutSecs) # wait the timeout amount before choosing a new target
 
 if __name__=="__main__":
@@ -171,22 +156,20 @@ if __name__=="__main__":
   # start tracker running and start following locations
   t = Tracker(args)
   from threading import Thread
-  thread = Thread(target=t.follow_mouth, args = (0.1,))
+  thread = Thread(target=t.follow_mouth, args = (0,))
   thread.start()
-
-  #rospy.spin()
-
-  pub = rospy.Publisher(point_topic, Point, queue_size=10)
+  poses =[ 
+                [ 0.21676946, -0.2959789,   0.552686472],
+                [ 0.21676946, -0.2959789,   0.52686472],
+                [ 0.21676946, -0.2959789,   0.42686472],
+                [ 0.31676946, -0.1959789,   0.42686472],
+                [ 0.322638  , -0.35740914,  0.43557774],
+                [ 0.31676946, -0.3959789,   0.42686472]]
+  pub = rospy.Publisher('feedbot/mouth_pose', tfMessage, queue_size=10)
   h = Header()
   h.stamp = rospy.Time.now()
-  times = np.array(range(100))
-  y_center = -0.195
-  z_center = 0.45
-  poses = [[ x_dist, y_center + 0.1 * np.sin(t),z_center + 0.1 * np.cos(t)] for t in times]
-
-  for pose in poses+poses+poses:
-    #mesg = tfMessage([TransformStamped(h, "dummy_frame", Transform(Vector3(pose[0], pose[1], pose[2]), None))])
-    mesg = Point(pose[0], pose[1], pose[2])
+  for pose in poses:
+    mesg = tfMessage([TransformStamped(h, "dummy_frame", Transform(Vector3(pose[0], pose[1], pose[2]), None))])
     pub.publish(mesg)
     rospy.sleep(1)
   thread.join()
